ee0c64a612de446230a9af3641d50382c03a9ae3
ftx <jay17791985208@163.com>
2020-07-27 22:33:27 +0800

    git第二次

--- test.go
+++ test.go
@@ -5,4 +5,10 @@ import "fmt"
 func main() {
 	fmt.Print("git")
 	fmt.Print("git")
+	fmt.Print("git")
+	fmt.Print("git")
+	fmt.Print("git1")
+	fmt.Print("git2")
+	fmt.Print("git3")
+	fmt.Print("git4")
 }
